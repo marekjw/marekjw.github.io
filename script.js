4de94682f9e29e8be9ec9053e6175a110ae07d82
marek <marekjwesolowski@gmail.com>
2022-02-28 21:38:45 +0100

    Add styles

---
+++ script.js
@@ -0,0 +1,4 @@
+function generate_text() {
+    var paragraph = document.getElementById("main-text");
+    paragraph.innerText = Math.random().toString();
+}
---
+++ script.ts
@@ -0,0 +1,4 @@
+function generate_text() {
+    const paragraph = document.getElementById("main-text")
+    paragraph.innerText = Math.random().toString()
+}
